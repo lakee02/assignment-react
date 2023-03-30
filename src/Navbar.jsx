6efeeb7a4f29251171fdab3123f33b8dce8aa3c9
lakee mangal <lakee.12002794@lpu.in>
2023-03-30 14:30:20 +0530

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,69 @@
+import React,{useState} from 'react'
+
+//import icon
+import {SiConsul} from 'react-icons/si';
+import {BsPhoneVibrate} from 'react-icons/bs';
+import {AiOutlineGlobal} from 'react-icons/ai';
+import {CgMenuGridO} from 'react-icons/cg'
+
+
+export const Navbar = () => {
+  // remove the navBar in the small width screen
+  const[active,setActive]=useState('navBarMenu')
+  const showNavBar=()=>{
+    setActive('navBarMenu showNavBar');
+  }
+  const removeNavBar=()=>{
+    setActive('navBarMenu');
+  }
+
+  // Add background color to the second Navbar
+  const[noBg,addBg]=useState('navBarTwo')
+  const addBgColor=()=>{
+    if(window.scrollY>=10){
+      addBg('navBarTwo navbar_With_Bg');
+    }else{
+      addBg('navBarTwo')
+    }
+  }
+  window.addEventListener('scroll',addBgColor)
+  
+  return (
+    <div className='navBar flex'>
+      <div className='navBarOne flex'>
+        <div>
+        <SiConsul className='icon'/>
+        </div>
+        <div className='none flex'>
+          <li className='flex'> <BsPhoneVibrate className='icon'/> Support</li>
+          <li className='flex'><AiOutlineGlobal className='icon'/> Languages</li>
+        </div>
+
+        <div className='atb flex'>
+          <span>Sign In</span>
+          <span>Sign Out</span>
+        </div>
+      </div>
+      <div className={noBg}>
+        <div className={active}>
+          <ul className='menu flex'>
+            <li onClick={removeNavBar} className='listItem'>Home</li>
+            <li onClick={removeNavBar} className='listItem'>About</li>
+            <li onClick={removeNavBar} className='listItem'>Offers</li>
+            <li onClick={removeNavBar} className='listItem'>Seats</li>
+            <li onClick={removeNavBar} className='listItem'>Destinations</li>
+          </ul>
+          <button onClick={removeNavBar} className='btn flex btnOne'>
+            Contact
+          </button>
+        </div>
+        <button className='btn flex btnTwo'>
+          Contact
+        </button>
+        <div onClick={showNavBar} className='toggleIcon'>
+        <CgMenuGridO className='icon'/>
+        </div>
+      </div>
+    </div>
+  )
+}
